403416c1459ade9a2c09f12dff306401119dbafb
ydalimir <ydalimir12arevalo16@outlook.es>
2021-06-09 11:38:28 -0500

    cambios

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdd119e2a58bdde8177bbeb314f01e26'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd0582147-a5b0-4f4d-80ee-23b3bc50ec2f'
+};
